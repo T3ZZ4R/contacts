8f081dd79328791a63edcfebd5bcd05f578861b6
Tezzar <alimimsoul@gmail.com>
2026-06-01 00:46:06 +0330

    adding selectors

---
+++ src/components/commun.js
@@ -0,0 +1,9 @@
+//global
+const searchBoxEl=document.querySelector("#searchInput");
+const nameBoxEl=document.querySelector("#nameInput");
+const numberBoxEl=document.querySelector("#phoneInput");
+const emailBoxEl=document.querySelector("#emailInput");
+const upImgEl=document.querySelector("#avatarInput");
+const addBtnEl=document.querySelector("#addContactBtn");
+const contactsFieldEl=document.querySelector("#contactsContainer");
+const emptyStateMsgEl=document.querySelector("#emptyState")
